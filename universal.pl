285006a0622de1ab3c5cb68debfd46d4b13ead12
James S White <james@jameswhite.org>
2010-04-24 15:43:41 -0500

    pasting in the input_event handler

--- universal.pl
+++ universal.pl
@@ -41,7 +41,7 @@ $poe->object_session(
                                             'port'            => '5222',
                                             'domain'          => 'websages.com',
                                             'username'        => 'crunchy',
-                                            'password'        => 'nottherealpassword',
+                                            'password'        => $ENV{'XMPP_PASSWORD'},
                                           }
                                         ), 
                     );
