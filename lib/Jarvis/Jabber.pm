78549d134dc50b493063f99d7165929fd4d77c51
James S White <james@jameswhite.org>
2010-04-26 11:47:00 -0500

    working on the pubmsg/privmsg routing for xmpp

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -211,7 +211,7 @@ sub input_event()
         print "1. " . $node->to_str() . "\n" if $self->{'DEBUG'} > 2;
         print "2. " . $node->get_id() . "\n" if $self->{'DEBUG'} > 2;
         print "3. " . ref($node) . "\n" if $self->{'DEBUG'} > 2;
-        print "4. " . join(" ", $node->getAttributes() ) . "\n" if $self->{'DEBUG'} > 2;
+        print "4. " . join(" ", $node->get_attrs() ) . "\n" if $self->{'DEBUG'} > 2;
         # allow everyone in websages to subscribe to our presence.
         if($node->name() eq 'presence'){
             #print Data::Dumper->Dump([$node->get_attrs()]) . "\n";
