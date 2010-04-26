76d87c39a087929899a7cfa33ed095e5d0056fcb
James S White <james@jameswhite.org>
2010-04-26 09:24:38 -0500

    xmpp only works with anonymous yields, trying to hard-code it's alias

--- lib/POE/Builder.pm
+++ lib/POE/Builder.pm
@@ -75,8 +75,8 @@ sub object_session(){
                           inline_states =>  {
                                               _start   => sub { 
                                                                 my ($kernel, $heap) = @_[KERNEL, HEAP];
-                                                                $kernel->post($_[SESSION],$object->alias()."_start");
                                                                 $kernel->alias_set( $object->alias() );
+                                                                $kernel->post($_[SESSION],$object->alias()."_start");
                                                                 print STDERR "******************************************\n";
                                                                 print STDERR Data::Dumper->Dump([$kernel->alias_list()])."\n";
                                                                 print STDERR "******************************************\n";
