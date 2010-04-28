b0ccaf15105e933ecc437a3887dbfc07c17bd1e2
James S White <james@jameswhite.org>
2010-04-27 19:20:03 -0500

    adding mediacas watcher to the mix

--- lib/Jarvis/Persona/Watcher.pm
+++ lib/Jarvis/Persona/Watcher.pm
@@ -69,6 +69,7 @@ sub _start{
      my $self = $_[OBJECT]||shift;
      my $kernel = $_[KERNEL];
      print STDERR __PACKAGE__ ." start\n";
+     $heap->{'twitter'}->yield('register');
      $kernel->delay($self->alias().'_delay_friend_timeline', 5);
      return $self;
 }
@@ -99,7 +100,7 @@ sub input{
 
 sub delay_friend_timeline {
     my($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
-    $kernel->post('friend_timeline');
+    $heap->{'twitter'}->yield('friend_timeline');
 }
 
 sub twitter_update_success {
