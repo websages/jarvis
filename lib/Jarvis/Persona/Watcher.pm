92a5e6847246c35c2efcbca418f4bfe7c38812ea
James S White <james@jameswhite.org>
2010-04-27 20:22:24 -0500

    trying to decypher the nested object event handling

--- lib/Jarvis/Persona/Watcher.pm
+++ lib/Jarvis/Persona/Watcher.pm
@@ -115,8 +115,10 @@ sub twitter_timeline_success {
     foreach my $tweet (@{ $ret }){
         #print "[\@". join("\n",keys(%{$tweet->{'user'}->{'screen_name}})) ."]: ".$tweet->{'text'}." ";
         my $text=$tweet->{'text'};
-        $text=~s/^I used #*Shazam to discover\s+(.*)\s+by\s+(.*)\s+http:\/\/.*/Does anyone have a copy of "$1" by $2?/;
-        $text=~s/^I used #*Shazam to discover\s+(.*)\s+by\s+(.*)\s+#shazam.*/Does anyone have a copy of "$1" by $2?/;
+        if($tweet->{'user'}->{'screen_name'} eq 'mediacas'){
+            $text=~s/^I used #*Shazam to discover\s+(.*)\s+by\s+(.*)\s+http:\/\/.*/$1 $2/;
+            $text=~s/^I used #*Shazam to discover\s+(.*)\s+by\s+(.*)\s+#shazam.*/$1 $2/;
+        }
         print "[\@".$tweet->{'user'}->{'screen_name'}."]: ".$text." ";
         print "\n";
         # print join("|", keys(%{ $tweet->{'user'} }))."\n";
