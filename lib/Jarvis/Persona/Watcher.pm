7399ac934553d3094f31f94f0299f47fe46c15a7
James S White <james@jameswhite.org>
2010-04-27 20:05:06 -0500

    trying to decypher the nested object event handling

--- lib/Jarvis/Persona/Watcher.pm
+++ lib/Jarvis/Persona/Watcher.pm
@@ -113,7 +113,7 @@ sub twitter_timeline_success {
     my($self, $kernel, $heap, $ret) = @_[OBJECT, KERNEL, HEAP, ARG0];
     print STDERR "twitter_timeline_success\n". ref($ret) ."\n";
     foreach my $tweet (@{ $ret }){
-        print "[\@". keys(%{$tweet->{'user'}}) ."]: ".$tweet->{'text'}." ";
+        print "[\@". join(":",keys(%{$tweet->{'user'}})) ."]: ".$tweet->{'text'}." ";
         print "\n";
         #print join("|", keys(%{ $tweet }))."\n";
         #source
