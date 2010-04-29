ef67e89345254b656bdb074dfaa5265e60c7fdd8
James S White <james@jameswhite.org>
2010-04-29 09:49:03 -0500

    trying to get jabber to join the group chats after chages broke it

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -284,7 +284,7 @@ sub output_event()
         print "\n===PACKET SENT===\n" if $self->{'DEBUG'} > 2;
         print $node->to_str() . "\n" if $self->{'DEBUG'} > 2;
         print "=================\n" if $self->{'DEBUG'} > 2;
-print STDERR "OUTPUT_HANDLER $room > ". $sid ."\n";
+print STDERR "post $sid output_handler, $node\n";
         $kernel->post($sid, 'output_handler', $node);
 }
 
