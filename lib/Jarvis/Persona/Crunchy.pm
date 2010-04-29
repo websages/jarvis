2c0743d630d0718774945c0bfbb01289c2f07e26
James White <whitejs@websages.com>
2010-04-28 22:14:04 -0500

    standings test

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -317,7 +317,7 @@ use HTML::Parser;
     my $h = sprintf "%-20s %-5s %-5s %-7s %-6s %-7s\n",
         '', 'W', 'L', ' Pct', 'GB', 'L10';
     #$arg{'kernel'}->post( localhost => privmsg => $d, $h);
-    print STDERR "$q";
+    print STDERR "$h";
     $p->parse( $content );
 }
 ################################################################################
