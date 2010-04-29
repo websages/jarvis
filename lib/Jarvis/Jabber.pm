53355913ebf45af4ef9c6e3a7ee6eb0f9a118835
James S White <james@jameswhite.org>
2010-04-29 09:20:10 -0500

    changing the behaviour of object_states to handle a list or hash

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -284,7 +284,6 @@ sub output_event()
         print "\n===PACKET SENT===\n" if $self->{'DEBUG'} > 2;
         print $node->to_str() . "\n" if $self->{'DEBUG'} > 2;
         print "=================\n" if $self->{'DEBUG'} > 2;
-        
         $kernel->post($sid, 'output_handler', $node);
 }
 
