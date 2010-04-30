fe7c3940087403bae35b5fad27413e66d474bf57
James S White <james@jameswhite.org>
2010-04-30 16:02:28 -0500

    tracking presences

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -276,9 +276,9 @@ sub input_event() {
             }else{                                  # add to $heap->{'prescense'}
                    $heap->{'presence'}->{ $node->attr('from') } = 
                     my $child_nodes = $node->get_children_hash();
-                    if(defined($child_nodes->{'x'})){
-                        print STDERR Data::Dumper->Dump([ $child_nodes->{'x'} ]);
-                    }
+                    #if(defined($child_nodes->{'x'})){
+                        print STDERR Data::Dumper->Dump([ $child_nodes ]);
+                    #}
             }
             print STDERR $self->pretty_xml( $node->to_str() );
         }
