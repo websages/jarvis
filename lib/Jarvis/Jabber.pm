c49065c89b8531127cbe304bb1c3be189baa42d2
James S White <james@jameswhite.org>
2010-04-30 12:13:51 -0500

    the type of room matters

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -244,7 +244,7 @@ sub input_event() {
         my  $twig= new XML::Twig;
         $twig->set_indent(" "x4);
         $twig->parse( $node->to_str() );
-        $twig->set_pretty_print( $sPrettyFormat );
+        $twig->set_pretty_print( 'indented' );
         print STDERR $twig->sprint;
 
         # don't parse things from this personality.
