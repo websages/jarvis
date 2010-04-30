7f19219d8c9f367ea818602ba65b6e07aee714e1
James S White <james@jameswhite.org>
2010-04-30 16:23:59 -0500

    tracking presences

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -122,7 +122,10 @@ sub authen {
     # we need to remember 
     push(@{ $heap->{'pending'} }, { 'authen' => $msg, 'sender' => $sender->ID } );
     #$self->{'irc_client'}->yield('whois', $msg->{'conversation'}->{'nick'} );
-    print Data::Dumper->Dump([$msg]);
+    if(defined( $heap->{'presence'}->{ $msg->{'conversation'}->{'room'}.'\\'.$msg->{'conversation'}->{'nick'} })){
+        #print Data::Dumper->Dump([$msg]);
+        print STDERR $heap->{'presence'}->{ $msg->{'conversation'}->{'room'}.'\\'.$msg->{'conversation'}->{'nick'} }->to_str()."\n";
+    }
     
     return;
 }
