6138ff2b6a3023c46e491ff5767b4dafaa991a4f
James S White <james@jameswhite.org>
2010-04-30 10:36:29 -0500

    Adding pseudo-authentication to the connectors

--- lib/Jarvis/IRC.pm
+++ lib/Jarvis/IRC.pm
@@ -268,7 +268,7 @@ sub irc_whois {
             if($reply->{'nick'} eq $request->{'authen'}->{'conversation'}->{'nick'}){
 print STDERR Data::Dumper->Dump([$request]);
 print STDERR $reply->{'nick'}." => ". $reply->{'user'} .'@'. $reply->{'host'} ."\n";
-                $kernel->post($reply->{'sender'}, 'authen_reply', $reply->{'user'} .'@'. $reply->{'host'});
+                $kernel->post($request->{'sender'}, 'authen_reply', $reply->{'user'} .'@'. $reply->{'host'});
             }else{
                 push(@{ $heap->{'pending'} }, $reply );
             }
