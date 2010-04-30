8cc6e48020a96b54b3d5030dc40244334fcfd596
James S White <james@jameswhite.org>
2010-04-30 10:27:41 -0500

    Adding pseudo-authentication to the connectors

--- lib/Jarvis/IRC.pm
+++ lib/Jarvis/IRC.pm
@@ -260,7 +260,7 @@ sub authen {
 sub irc_whois {
     my ($self, $kernel, $heap, $sender, $reply) = @_[OBJECT, KERNEL, HEAP, SENDER, ARG0];
     # look through our pending requests for authen
-print STDERR Data::Dumper->Dump([ $sender->alias() ]);
+print STDERR Data::Dumper->Dump([ $sender ]);
     my $max=$#{ $heap->{'pending'} };
     my $count=0;
     while ($count++ <= $max){
