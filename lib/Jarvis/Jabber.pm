5e46ef9d8db37f40ef0ca327bdd75384c4f83bae
James S White <james@jameswhite.org>
2010-05-01 11:49:24 -0500

    tracking presences

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -289,7 +289,6 @@ sub input_event() {
 
             my $child_nodes = $node->get_children_hash();
             if(defined($child_nodes->{'x'}) && (ref($child_nodes->{'x'}) eq 'POE::Filter::XML::Node')){
-print STDERR "[ ".ref($child_nodes->{'x'}) ." ]\n";
                 my $child_child_nodes = $child_nodes->{'x'}->get_children_hash();
                 if(defined($child_child_nodes->{'item'}) && (ref($child_nodes->{'x'}) eq 'POE::Filter::XML::Node')){
                     if( $child_child_nodes->{'item'}->attr('jid') ){
