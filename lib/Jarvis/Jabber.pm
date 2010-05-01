70f771ffa4d3efaf44de36781d4c0c65457de580
James S White <james@jameswhite.org>
2010-04-30 21:48:49 -0500

    tracking presences

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -264,7 +264,7 @@ sub input_event() {
     my ($self, $kernel, $heap, $node) = @_[OBJECT, KERNEL, HEAP, ARG0];
     #print "\n\nINCOMING:\n".$node->to_str()."\n\n\n";
 
-    print STDERR $node->to_str()."\n\n";
+    print STDERR Data::Dumper->Dump([$node]);
 
     # allow everyone in websages to subscribe to our presence. /*FIXME move regex to constructor */
     if($node->name() eq 'presence'){
