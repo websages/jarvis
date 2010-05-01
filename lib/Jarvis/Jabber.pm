79dfd8187178b574129fd953da7b067b06f88ecf
James S White <james@jameswhite.org>
2010-05-01 11:03:58 -0500

    tracking presences

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -213,8 +213,8 @@ sub status_event()
                     }
                 }
    
-                # Ignore incoming messages for 10 seconds, so we don't re-respond to crap in the replay buffer
-                $kernel->delay_add('enable_input', 10);
+                # Ignore incoming messages for 3 seconds, so we don't re-respond to crap in the replay buffer
+                $kernel->delay_add('enable_input', 3);
 
                 #for(1..10)
                 #{
