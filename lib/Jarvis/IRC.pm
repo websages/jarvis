e45aa185c1b5e2b49dfbe4c41d0cd7525f732227
James S White <james@jameswhite.org>
2010-05-01 16:44:14 -0500

    adding quote

--- lib/Jarvis/IRC.pm
+++ lib/Jarvis/IRC.pm
@@ -194,9 +194,11 @@ sub irc_public {
     #log everything before we do anything with it.
     $_[KERNEL]->post('logger', 'log', "$channel <$nick> $what");
 
+print STDERR "( $what )\n";
     $what=~s/[^a-zA-Z0-9:!\@#\%^&*\[\]_+=\- ]//g;
     $what=~s/[\$\`\(]//g;
     $what=~s/[)]//g;
+print STDERR "< $what >\n";
     my $msg = { 
                 'sender_alias' => $self->alias(),
                 'reply_event'  => 'irc_public_reply',
