e024eed395983d3b6c57d97e11e4d831467811c5
James S White <james@jameswhite.org>
2010-05-01 18:37:26 -0500

    adding advanced tell

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -142,6 +142,7 @@ sub authen_reply{
                 }
             }
         }elsif($msg->{'reason'} eq 'tell_request'){ 
+            $user=~s/\@.*//;
 print STDERR Data::Dumper->Dump([$user,$msg]);
             # if the nick didn't translate to a userid, they may not be logged in, 
             # but the request may have been for a userid, so let's try to look that up...
