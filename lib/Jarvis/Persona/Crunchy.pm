6b860a852bc914d4402cfcc3210931b2a8a41035
James S White <james@jameswhite.org>
2010-05-01 16:56:42 -0500

    adding quote

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -303,8 +303,8 @@ sub quote{
     return undef unless $quote;
     return undef unless $author;
 
-    $quote  =~ s/</\&3e/g;  $author =~ s/</\%3c/g;
-    $quote  =~ s/>/\&3c/g;  $author =~ s/>/\&3e/g;
+    $quote  =~ s/</\%3e/g;  $author =~ s/</\%3c/g;
+    $quote  =~ s/>/\%3c/g;  $author =~ s/>/\%3e/g;
     $quote  =~ s/\&/\%26/g; $author =~ s/\&/\%26/g;
     $quote  =~ s/;/\%3b/g;  $author =~ s/\;/\%3b/g;
 
