ceda7cbc4afe6a193fa10d3ffded2634c5805439
James S White <james@jameswhite.org>
2010-05-02 10:16:19 -0500

    adding terminate crunchy

--- lib/Jarvis/IRC.pm
+++ lib/Jarvis/IRC.pm
@@ -99,8 +99,14 @@ sub start {
 sub stop  { 
     my $self = $_[OBJECT]; 
     my $kernel = $_[KERNEL];
-    print STDERR ref($self)." stopped.\n"; 
-    $self->{'irc_client'}->yield( disconnect => {} );
+    $self->{'irc_client'}->yield('disconnect');
+    if($self->{'irc_client'}->yield('connected')){
+        print STDERR ref($self)." disconnected.\n"; 
+    }else{
+        print STDERR ref($self)." failed to disconnect.\n"; 
+    }
+  
+    $self->{'irc_client'}->yield( unregister => 'all' );
 }
 
 sub states { my $self = $_[OBJECT]; return $self->{'states'};           }
