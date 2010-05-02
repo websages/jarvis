d772353274a49ff87aebb5f24f3bd0b9390b6546
James S White <james@jameswhite.org>
2010-05-01 20:37:35 -0500

    adding shoutout toggle

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -539,6 +539,7 @@ sub toggle_shoutout{
         }
         if($modified == 1){
             $entry->replace('uniqueMember' => @users);    
+print STDERR Data::Dumper->Dump([@users]);
 print STDERR Data::Dumper->Dump([$entry]);
             $self->update({'entry' => $entry});
             $r = "cn=shoutouts modified.";
