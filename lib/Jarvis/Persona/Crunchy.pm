751e9d5d575dbe00fd30de26a8d31c2de294b90e
James S White <james@jameswhite.org>
2010-05-03 09:35:05 -0500

    shoutout crash debugging

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -600,8 +600,9 @@ sub shoutout{
     my $originator=shift if @_;
     my @list;
     return "shoutout what?" unless $shoutout;
-print STDERR Data::Dumper->Dump([$self->get_ldap_entry("(cn=shoutouts)")]);
-    foreach my $entry ( $self->get_ldap_entry("(cn=shoutouts)") ){
+    my $shoutouts = $self->get_ldap_entry("(cn=shoutouts)");
+    return $self->error(unless ($shoutouts));
+    foreach my $entry (@{ $shoutouts }){
         my @users = $entry->get_value('uniqueMember');
         foreach my $user (@users){
             $user=~s/,.*//;
