f63d16fade558d3b73732ee6c5f85f501a0c1637
James S White <james@jameswhite.org>
2010-05-07 12:16:46 -0500

    authen_reply isn't replying...

--- lib/Jarvis/Persona/Base.pm
+++ lib/Jarvis/Persona/Base.pm
@@ -16,8 +16,8 @@ sub new {
     $self->{'must'} = [ 'alias' ];
     # an optional overloaded subroutine can provide more.
     my $oload_list=$self->must();
-    while(my $oload_may = shift @{ $oload_list }){
-        push(@{ $self->{'must'} }, $oload_may );
+    while(my $oload_must = shift @{ $oload_list }){
+        push(@{ $self->{'must'} }, $oload_must );
     }
 
     # hash of optional constructor elements (key), and their default (value) if not specified
@@ -27,7 +27,7 @@ sub new {
     foreach my $key (keys(%{ $oload_hash })){
         $self->{'may'}->{ $key } = $oload_hash->{ $key };
     }
-
+print Data::Dumper->Dump([$self->{'may'}]);
     # set our required values fron the constructor or the defaults
     foreach my $attr (@{ $self->{'must'} }){
          if(defined($construct->{$attr})){
