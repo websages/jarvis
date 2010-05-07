9ee4a68cb6a4ddd4853ac522f6b89e32961ea06d
James S White <james@jameswhite.org>
2010-05-07 08:52:24 -0500

    debugging ejabberd disconnects

--- universal.pl
+++ universal.pl
@@ -60,7 +60,7 @@ my $xmpp_connection = << "...";
 class: Jarvis::Jabber
 init: 
   alias: ${hostname}_xmpp
-  ip: 127.0.0.1
+  ip: thor.websages.com
   port: 5222
   hostname: ${domain}
   username: crunchy
