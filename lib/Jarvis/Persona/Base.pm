d94a3843de5afd43baf0d4e4da6d004317679abc
James S White <james@jameswhite.org>
2010-05-07 10:48:44 -0500

    completely need to re-think the input handler...

--- lib/Jarvis/Persona/Base.pm
+++ lib/Jarvis/Persona/Base.pm
@@ -219,27 +219,4 @@ sub input{
     return $self->{'alias'};
 }
 
-################################################################################
-# Here is what you must provide: 
-#   A function named "input_handler" that takes $what and $directly_addressed
-#   as arguments, that will regex out the appropriate commands and act on them.
-#   It should return a list reference to the list of one-line replies.
-#   You will also need to subroutines or inline code to handle these actions.
-################################################################################
-
-sub input_handler{
-    my $self=shift;
-    my $line=shift;
-    my $direct=shift||0;
-    for ( $line ) {
-        /^\s*!*help\s*/          && return $self->help();
-        /.*/                     && return $self->{'megahal'}->do_reply($what);
-    }
-}
-
-sub help(){
-    my $self=shift;
-    return  [ "I need a help routine" ];
-}
-
 1;
