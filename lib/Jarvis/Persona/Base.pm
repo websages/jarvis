38e11e35bb0ae4280844831ef3d5b9029eb9794f
James S White <james@jameswhite.org>
2010-05-07 11:58:34 -0500

    authen_reply isn't replying...

--- lib/Jarvis/Persona/Base.pm
+++ lib/Jarvis/Persona/Base.pm
@@ -58,6 +58,7 @@ sub new {
                           'channel_del'   => 'channel_del',
                         };
     my $pstates = $self->persona_states(); 
+print DATA::Dumper->Dump([$pstates}]);
     if(ref($pstates) eq 'HASH'){
         # the standard case returning a hash of { 'event' => 'subname' }
         foreach my $key (keys(%{ $pstates })){
@@ -70,6 +71,7 @@ sub new {
             $self->{'states'}->{ $key } = $key;
         }
     }
+print DATA::Dumper->Dump([$self->{'states'}]);
     return $self;
 }
 
