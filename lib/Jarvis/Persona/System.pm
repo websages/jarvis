5706323e2b94efb060143417cc85e954b4628303
James White <whitejs@websages.com>
2010-05-28 14:21:46 -0500

    why is twitter broken

--- lib/Jarvis/Persona/System.pm
+++ lib/Jarvis/Persona/System.pm
@@ -266,7 +266,7 @@ sub spawn{
     }
     foreach my $p (@{ $self->{'known_personas'} }){
         if($p->{'name'} eq $persona){
-            my $poe = new POE::Builder({ 'debug' => '0','trace' => '0' });
+            my $poe = new POE::Builder({ 'debug' => '0','trace' => '1' });
             return undef unless $poe;
             
             $poe->object_session( $p->{'persona'}->{'class'}->new( $p->{'persona'}->{'init'} ) );
