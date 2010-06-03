c9317ba519c88e2a9f13736b44e4591989cb7252
James White <whitejs@websages.com>
2010-06-03 17:38:54 -0500

    fixing shoutouts

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -525,9 +525,9 @@ sub shoutout{
     my @list;
     return "shoutout what?" unless $shoutout;
     my $shoutouts;
+    print STDERR Data::Dumper->Dump([$shoutouts]);
     push(@{ $shoutouts },$self->get_ldap_entry("(cn=shoutouts)"));
     return $self->error() unless defined($shoutouts);
-    print STDERR Data::Dumper->Dump([$shoutouts]);
     foreach my $entry (@{ $shoutouts }){
         my @users = $entry->get_value('uniqueMember');
         foreach my $user (@users){
