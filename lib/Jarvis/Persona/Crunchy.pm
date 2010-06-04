8668a9742fd33dc130940563ffb0b401d37cda72
James S White <james@jameswhite.org>
2010-06-04 09:52:11 -0500

    adding whois

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -160,6 +160,15 @@ sub input{
                                                 $kernel->post($sender, 'authen', $msg);
                                                 last;
                                               };
+            /^\s*who\s+the\s+fuck\s+is\s+(\S+)[\?\s]*/    && do {
+                                                my $target=$1;
+                                                $target=~s/\?$//;
+                                                $pirate=0;
+                                                $msg->{'reason'}='whois';
+                                                $msg->{'conversation'}->{'nick'}=$target;
+                                                $kernel->post($sender, 'authen', $msg);
+                                                last;
+                                              };
             /.*/                        && do { $replies = [ $self->megahal($what) ] if($direct);      last; };
             /.*/                        && do { last; };
         }
