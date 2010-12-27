96e3eb9cb69d96feef965a22b19a4a24823d04a4
James S White <james@jameswhite.org>
2010-12-27 15:41:38 -0600

    debugging xmpp invite events

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -226,6 +226,7 @@ sub is_invite{
         if(ref($child_nodes) ne "ARRAY"){ $child_nodes = [ $child_nodes ]; } # forcearray
         foreach my $cnode( @{ $child_nodes } ){
             print ref($cnode)."\n";
+            print Data:Dumper->Dump([ $cnode ])."\n";
             return $self->is_invite($cnode) if $self->is_invite($cnode);
         }
     }else{
