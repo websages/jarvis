1b87e21d136380825cf187e4764a8466e85de39d
James S White <james@jameswhite.org>
2010-12-27 13:03:46 -0600

    debugging xmpp invite events

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -255,7 +255,7 @@ sub input_event() {
             }
         }
     }elsif($node->name() eq 'message'){
-        print STDERR Data::Dumper->Dump([ $node->attr() ])."\n";
+        print STDERR Data::Dumper->Dump([ ref($node) ])."\n";
     }else{
         print STDERR "-=[ node->name: " .$node->name()."]=-\n";
     }
