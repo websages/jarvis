f508ea0bc128d1571803bd3fa0f46fbd79f80a79
James S White <james@jameswhite.org>
2010-12-27 13:36:17 -0600

    debugging xmpp invite events

--- lib/Jarvis/Jabber.pm
+++ lib/Jarvis/Jabber.pm
@@ -222,14 +222,15 @@ sub is_invite{
     my $self=shift;
     my $node=shift if(@_);
     if($node->name() eq 'message'){
+        print STDERR $node->to_str()."\n";
         my $child_nodes = $node->get_children_hash();
-        if(defined($child_nodes->{'x'}) && (ref($child_nodes->{'x'}) eq 'POE::Filter::XML::Node')){
-            my $child_child_nodes = $child_nodes->{'x'}->get_children_hash();
-            print Data::Dumper->Dump([ keys(%{$child_child_nodes} )]);
-            return 1;
-        }else{
-            print Data::Dumper->Dump([ keys(%{$child_nodes} )]);
-        }
+        #if(defined($child_nodes->{'x'}) && (ref($child_nodes->{'x'}) eq 'POE::Filter::XML::Node')){
+        #    my $child_child_nodes = $child_nodes->{'x'}->get_children_hash();
+        #    print Data::Dumper->Dump([ keys(%{$child_child_nodes} )]);
+        #    return 1;
+        #}else{
+        #    print Data::Dumper->Dump([ keys(%{$child_nodes} )]);
+        #}
     }
     return undef;
 }
