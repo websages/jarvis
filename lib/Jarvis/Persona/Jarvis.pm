b2eb0137c09a055f05616d7145ae47467ffec1e5
James S White <james@jameswhite.org>
2011-04-19 12:36:38 -0500

    Re-designing the persona/connector relationship

--- lib/Jarvis/Persona/Jarvis.pm
+++ lib/Jarvis/Persona/Jarvis.pm
@@ -82,6 +82,7 @@ print Data::Dumper->Dump([$msg]);
                 if( defined($line) && ($line ne "") ){ 
                     $kernel->post($sender, $respond_event, $msg, $who.': '.$line); 
                     if(ref($where) eq 'ARRAY'){ $where = $where->[0]; }
+                    $kernel->post($self->{'logger'}, 'log', "#privmsg <$who> $what");
                     $kernel->post($self->{'logger'}, 'log', "#privmsg <$where> $who: $line");
                 }else{
                     $kernel->post($self->{'logger'}, 'log', "$where <$nick> $who: $line");
