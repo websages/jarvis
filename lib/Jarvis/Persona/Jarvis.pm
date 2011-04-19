b7b170e807cb87479dd2f1c007725d9db82c329b
James S White <james@jameswhite.org>
2011-04-19 11:18:23 -0500

    Re-designing the persona/connector relationship

--- lib/Jarvis/Persona/Jarvis.pm
+++ lib/Jarvis/Persona/Jarvis.pm
@@ -62,48 +62,6 @@ sub alias{
 }
 
 ################################################################################
-# let the personality know that the connector is now watching a channel
-################################################################################
-sub channel_add{
-     # expects a constructor hash of { alias => <sender_alias>, channel => <some_tag>, nick => <nick in channel> }
-    my ($self, $kernel, $heap, $construct) = @_[OBJECT, KERNEL, HEAP, ARG0];
-         push ( 
-                @{ $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} } },
-                $construct->{'nick'}
-              );
-}
-
-################################################################################
-# let the personality know that the connector is no longer watching a channel
-################################################################################
-sub channel_del{
-    # expects a constructor hash of { alias => <sender_alias>, channel => <some_tag>, nick => <nick in channel> }
-    my ($self, $kernel, $heap, $construct) = @_[OBJECT, KERNEL, HEAP, ARG0];
-    # unshift each of the items in the room, push them back if they're not the one we're removing
-    my $count=0;
-    my $max = $#{ $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} } };
-    while( $count < $max ){
-       my $nick = shift(@{ $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} } });
-        push( 
-              @{ $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} } },
-              $nick
-            ) unless $nick eq $construct->{'nick'};
-        $count++;
-   }
-    # delete the channel if there are no nicks in it
-    if($heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} }){
-        if($#{ $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} } } < 0){
-            delete $heap->{'locations'}->{ $construct->{'alias'} }->{ $construct->{'channel'} };   
-        }
-        # delete the alias from locations if there are no channels in it
-        if($heap->{'locations'}->{ $construct->{'alias'} }){
-            my @channels = keys(%{ $heap->{'locations'}->{ $construct->{'alias'} } });
-            if($#channels < 0){ delete $heap->{'locations'}->{ $construct->{'alias'} }; }
-        }
-    }
-}
-
-################################################################################
 # the messages get routed here from the connectors, a reply is formed, and 
 # posted back to the sender_alias,reply event (this function will need to be
 # overloaded...
