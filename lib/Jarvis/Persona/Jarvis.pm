b52a8ffb40ca2b83a6f3035d25aca949baddf6f4
James S White <james@jameswhite.org>
2011-04-19 11:08:58 -0500

    Re-designing the persona/connector relationship

--- lib/Jarvis/Persona/Jarvis.pm
+++ lib/Jarvis/Persona/Jarvis.pm
@@ -18,30 +18,6 @@ sub may {
 
 sub persona_start{
     my ($self, $kernel, $heap, $sender, @args) = @_[OBJECT, KERNEL, HEAP, SENDER, ARG0 .. $#_];
-    if(-d "/var/log/irc"){    $self->{'logdir'} = "/var/log/irc"    unless $self->{'logdir'}; }
-    if(-d "/var/log/irclog"){ $self->{'logdir'} = "/var/log/irclog" unless $self->{'logdir'}; }
-    if($self->{'logdir'}){
-        $self->{'logger'}  = POE::Component::Logger->spawn(
-                                 ConfigFile => Log::Dispatch::Config->configure(
-                                     Log::Dispatch::Configurator::Hardwired->new(
-                                         # convert me to yaml and put me in the main config
-                                         {
-                                           'file'   => {
-                                                         'class'    => 'Log::Dispatch::File',
-                                                         'min_level'=> 'debug',
-                                                         'filename' => "$self->{'logdir'}/channel.log",
-                                                         'mode'     => 'append',
-                                                         'format'   => '%d{%Y%m%d %H:%M:%S} %m %n',
-                                                       },
-                                           'screen' => {
-                                                          'class'    => 'Log::Dispatch::Screen',
-                                                          'min_level'=> 'info',
-                                                          'stderr'   => 0,
-                                                          'format'   => '%m',
-                                                       }
-                                          }
-                                     )), 'log') or warn "Cannot start Logging $!";
-    }
     return $self;
 }
 
