53312a631e02c4fca4daaa79cbd500cf8590cc92
James S White <james@jameswhite.org>
2011-04-19 10:02:06 -0500

    Re-designing the persona/connector relationship

--- lib/Jarvis/Persona/Base.pm
+++ lib/Jarvis/Persona/Base.pm
@@ -309,6 +309,7 @@ sub connector{
     foreach my $conn (@args){
         my $poe = new POE::Builder({ 'debug' => $self->{'debug'} ,'trace' => $self->{'trace'} });
         my $new_session_id = $poe->object_session( $conn->{'class'}->new( $conn->{'init'} ) );
+        print STDERR "::> $new_session_id\n";
         push(@{ $self->{'connectors'} }, $new_session_id);
     }
 }
