5e98f12cb36a156cea4825ef1a93fe745a1be7e7
James S White <james@jameswhite.org>
2011-04-19 09:33:26 -0500

    cannot find alias set?

--- lib/POE/Builder.pm
+++ lib/POE/Builder.pm
@@ -132,5 +132,6 @@ sub object_session(){
                                             },
                           heap           => { $object->alias() => $object }
                     ));
+    print STDERR "Created: ".$self->{'sessions'}->[$#{ $self->{'sessions'} }]->ID."\n";
 }
 1;
