804347679d65a3f1b6f0b2d415a95e6434b401da
James S White <james@jameswhite.org>
2011-04-20 12:40:51 -0500

    cmdb integration to jarvis

--- lib/Jarvis/Persona/Jarvis.pm
+++ lib/Jarvis/Persona/Jarvis.pm
@@ -177,9 +177,9 @@ sub sets{
                                         });
 print STDERR "[ $top ]\n";
     print STDERR Data::Dumper->Dump([ $self->{'groups'}->sets_in($top) ];
-    foreach my $set (@sets){
-        $kernel->post($msg->{'sender_alias'},$msg->{'reply_event'}, $msg, $set);
-    }
+    #foreach my $set (@sets){
+    #    $kernel->post($msg->{'sender_alias'},$msg->{'reply_event'}, $msg, $set);
+    #}
 }
 
 sub gist{
