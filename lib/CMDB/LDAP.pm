08ae2c598e4413f149a6977648abbd643a7ceb46
James S White <james@jameswhite.org>
2011-04-20 13:14:29 -0500

    cmdb integration to jarvis

--- lib/CMDB/LDAP.pm
+++ lib/CMDB/LDAP.pm
@@ -408,16 +408,13 @@ sub all_sets{
 
 sub sets_in{
     my $self = shift;
-print Data::Dumper->Dump([@_]);
     my $parent = shift if @_;
     my @tops;
     if($parent){
-print STDERR "--------------------------------------> $parent\n";
+print STDERR "\n\n".$self->set2dn($parent)."\n\n";
         foreach my $set (@{ $self->all_sets() }){
             if($set=~m/^${parent}::/){
-print STDERR "                      ----------------> $set\n";
                 $set=~s/^${parent}:://;
-print STDERR "                      ----------------> $set\n";
                 my @tmp = split(/::/, $set );
                 my $top = shift( @tmp );
                 push(@tops,$top) unless grep(/$top/,@tops);
