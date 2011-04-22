3a4133ed3a66506285acd9c41119b291a810beeb
James S White <james@jameswhite.org>
2011-04-22 11:45:34 -0500

    cmdb integration to jarvis

--- lib/Jarvis/Persona/Jarvis.pm
+++ lib/Jarvis/Persona/Jarvis.pm
@@ -162,25 +162,25 @@ sub input{
             /^\s*hello\s+$nick\s*/i && 
                 do { $replies = [ "hello $who" ]; last; };
             /^\s*hello\s*$/i && 
-                do { $replies = [ "hello" ] if ($addressed|$direct == 1); last; };
+                do { $replies = [ "hello" ] if (($addressed|$direct) == 1); last; };
             /^\s*good\s+(morning|day|afternoon|evening|night)\s+$nick\s*/i && 
-                do { $replies = [ "good $1 $who" ] if ($addressed|$direct == 1); last; };
+                do { $replies = [ "good $1 $who" ] if (($addressed|$direct) == 1); last; };
             /^\s*good\s+(morning|day|evening"afternoon||night)/i && 
-                do { $replies = [ "good $1 $who" ] if ($addressed|$direct == 1); last; };
+                do { $replies = [ "good $1 $who" ] if (($addressed|$direct) == 1); last; };
         # Thanks
             /^\s*(thanks|thank you|thx|ty)\s+$nick\s*/i && 
                 do { $replies = [ "np" ]; last; };
             /^\s*(thanks|thank you|thx|ty)/i && 
-                do { $replies = [ "np" ] if ($addressed|$direct == 1); last; };
+                do { $replies = [ "np" ] if (($addressed|$direct) == 1); last; };
         ########################################################################
         # Thanks
             /^\s*(thanks|thank you|thx|ty)\s+$nick\s*/i && 
                 do { $replies = [ "np" ]; last; };
             /^\s*(thanks|thank you|thx|ty)/i && 
-                do { $replies = [ "np" ] if ($addressed|$direct == 1); last; };
+                do { $replies = [ "np" ] if (($addressed|$direct) == 1); last; };
         ########################################################################
             /.*/ && 
-                 do { $replies = [ "i don't understand"] if($addressed|$direct == 1); last; };
+                 do { $replies = [ "i don't understand"] if(($addressed|$direct) == 1); last; };
         ########################################################################
             /.*/ 
                  && do { last; }
