030e8e336e68d7cd51594f49536fb133b44fb0ef
James S White <james@jameswhite.org>
2011-04-22 14:27:59 -0500

    cmdb integration to jarvis

--- lib/CMDB/LDAP.pm
+++ lib/CMDB/LDAP.pm
@@ -400,8 +400,7 @@ sub all_sets{
         $entry_dn=~s/,ou=$self->{'setou'}.*//;
 
         $entry_dn = join( ',',( reverse( split(/,/,$entry_dn))));
-        $entry_dn=~s/^ou=/:/;
-        $entry_dn=~s/^cn=//;
+        $entry_dn=~s/^(cn|ou)=(.*)/$2::/;
         $entry_dn=~s/,\s*(cn|ou)=/::/g;
         push(@{ $sets }, $entry_dn);
     }
