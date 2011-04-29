8108a89bd1fb0bc9a2f7c41f58f91f055e1f542b
James S White <james@jameswhite.org>
2011-04-29 14:05:37 -0500

    undefined object check

--- lib/CMDB/LDAP.pm
+++ lib/CMDB/LDAP.pm
@@ -307,6 +307,7 @@ sub ldap_bind{
             $server=$1 if ($server=~m/(^[A-Za-z0-9\-\.\/:]+$)/);
         }
         $self->{'ldap'} = Net::LDAP->new($server) || warn "could not connect to $server $@";
+        return undef unless defined($self->{'ldap'});
         if(defined($self->binddn) && defined($self->bindpw)){
             $mesg = $self->{'ldap'}->bind( $self->binddn, password => $self->bindpw );
         }else{
