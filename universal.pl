a3e9f940b45c1f25822adeb0707af5780a29870f
James S White <james@jameswhite.org>
2011-05-02 11:15:08 -0500

    config-file re-org

--- universal.pl
+++ universal.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 $ENV{'PATH'}='/usr/local/bin:/usr/bin:/bin';
 $ENV{'IFS'}=' \t\n';
+################################################################################
+# Add local libraries
 BEGIN { 
         use Cwd;
         my $path=$0;
@@ -18,14 +20,8 @@ BEGIN {
         # add all of these to our library search path
         foreach my $dir (@{$libdirs}){ unshift @INC, $dir if -d $dir; };
       }
-
-# disable jabber if we have no xmpp creds
-my $enable_jabber=1;
-if(!defined($ENV{'XMPP_PASSWORD'})){
-    $enable_jabber=0;
-    print STDERR "no XMPP_PASSWORD, disabling XMPP\n";
-}
-
+################################################################################
+# Include our dependencies
 use Data::Dumper;
 use Jarvis::IRC;
 use Jarvis::Jabber;
@@ -37,79 +33,54 @@ use Jarvis::Persona::Jarvis;
 use POE::Builder;
 use Sys::Hostname::Long;
 use Template;
-#daemonize();
-
-$|++;
-
-# get our fqd, hostname, and domain name
-my $fqdn     = hostname_long;
-my $hostname = $fqdn;         $hostname=~s/\..*$//;
-my $domain   = $fqdn;         $domain=~s/^[^\.]*\.//;
-
-# get a handle for our builder
-my $poe = new POE::Builder({ 'debug' => '0','trace' => '0' });
-exit unless $poe;
-
-########################################
-# define the system personality 
-# (so we have something to talk to)
-########################################
-my $persona = << "...";
----
-class: Jarvis::Persona::System
-init: 
-  alias: system
-  trace: 1
-  peer_group: cn=bot_managed
-  ldap_bindpw: ${ENV{'SECRET'}}
-...
-
-########################################
-# definition to connect to irc
-########################################
-my $irc_connection = << "...";
----
-class: Jarvis::IRC
-init:
-  alias: ${hostname}_irc
-  nickname: ${hostname}
-  ircname: ${fqdn}
-  server: 127.0.0.1
-  domain: ${domain}
-  channel_list:
-    - #asgard
-  persona: system
-...
-
-########################################
-# definition to connect to ejabberd
-########################################
-my $xmpp_connection = << "...";
----
-class: Jarvis::Jabber
-init: 
-  alias: ${hostname}_xmpp
-  ip: ${hostname}.${domain}
-  port: 5222
-  hostname: ${domain}
-  username: ${hostname}
-  password: ${ENV{'XMPP_PASSWORD'}}
-  channel_list: 
-    - asgard\@conference.websages.com/${hostname}
-  persona: system
-...
-
+use Cwd;
 ################################################################################
-# Do the work
-################################################################################
-$poe->yaml_sess($persona);
-$poe->yaml_sess($irc_connection);
-$poe->yaml_sess($xmpp_connection) if($jabber_enabled == 1);
-# fire up the kernel
-POE::Kernel->run();
-
 sub daemonize {
     defined( my $pid = fork() ) or die "Can't fork: $!\n";
     exit if $pid;
     setsid or die "Can't start a new session: $!\n";
 }
+################################################################################
+$|++;
+my $path=$0; $path=~s/\/[^\/]*$//; chdir($path); my $personas=cwd()."persona.d";
+# disable jabber if we have no xmpp creds
+my $enable_jabber=1;
+if(!defined($ENV{'XMPP_PASSWORD'})){
+    $enable_jabber=0;
+    print STDERR "no XMPP_PASSWORD, disabling XMPP\n";
+}
+
+# get a handle for our builder
+my $poe = new POE::Builder({ 'debug' => '0','trace' => '0' });
+exit unless $poe;
+
+# Template our YAML configs
+my ($persona, $irc_connection, $xmpp_connection);
+my $config = {
+               INCLUDE_PATH => $personas,  # or list ref
+               INTERPOLATE  => 1,               # expand "$var" in plain text
+               POST_CHOMP   => 1,               # cleanup whitespace 
+               PRE_PROCESS  => 'header',        # prefix each template
+               EVAL_PERL    => 1,               # evaluate Perl code blocks
+             };
+
+# get our fqd, hostname, and domain name
+my $fqdn     = hostname_long;
+my $hostname = $fqdn;         $hostname=~s/\..*$//;
+my $domain   = $fqdn;         $domain=~s/^[^\.]*\.//;
+my $vars = {
+               'SECRET'        => ${ENV{'SECRET'}},
+               'HOSTNAME'      => $hostname;
+               'FQDN'          => $fqdn;
+               'IRC_SERVER'    => '127.0.0.1',
+               'DOMAIN'        => $domain,
+               'XMPP_PASSWORD' => ${ENV{'XMPP_PASSWORD'}},
+           };
+# Set up our sessions 
+$template->process('system', $vars, \$persona);              $poe->yaml_sess($persona);
+$template->process('system_irc', $vars, \$irc_connection);   $poe->yaml_sess($irc_connection);
+$template->process('system_xmpp', $vars, \$xmpp_connection); $poe->yaml_sess($xmpp_connection) if($jabber_enabled == 1);
+
+# fire up the kernel
+POE::Kernel->run();
+
