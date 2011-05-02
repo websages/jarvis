74f98de02a351b95e49fe7e7fb205b3652b45643
James S White <james@jameswhite.org>
2011-05-02 13:21:47 -0500

    refactoring personas

--- lib/Jarvis/Persona/System.pm
+++ lib/Jarvis/Persona/System.pm
@@ -14,7 +14,10 @@ sub known_personas{
     my $self=shift;
     my $host=hostname_long(); $host=~s/\..*//g;
     push(@{ $self->{'persona_dirs'} },"/etc/jarvis/personas.d");
-    my $path=$0; $path=~s/\/[^\/]*$//; chdir($path); 
+    my $path=$0; 
+    print STDERR "PATH: $path\n";
+    $path=~s/\/[^\/]*$//; 
+    chdir($path); 
     push(@{ $self->{'persona_dirs'} },cwd()."/persona.d");
     foreach my $dir (@{ $self->{'persona_dirs'} }){
         print STDERR ":: $dir ::\n";
