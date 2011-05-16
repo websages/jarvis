549b7978de4de5e8e1fdc2c2b6ccf3c9c5b05620
James S White <james@jameswhite.org>
2011-05-16 14:58:17 -0500

    tracing ldap_uri through the sewers

--- lib/Jarvis/Persona/System.pm
+++ lib/Jarvis/Persona/System.pm
@@ -255,6 +255,7 @@ sub spawn{
         if($p->{'name'} eq $persona){
             my $poe = new POE::Builder({ 'debug' => '0','trace' => '0' });
             return undef unless $poe;
+print STDERR Data::Dumper->Dump([$p]);
             $poe->object_session( $p->{'persona'}->{'class'}->new( $p->{'persona'}->{'init'} ) );
             push( @{ $self->{'spawned'}->{$persona} }, $p->{'persona'}->{'init'}->{'alias'} );
             
