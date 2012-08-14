88745c421de681edd96d5cb518d0c096c4a23522
James S White <james@jameswhite.org>
2012-08-14 16:49:01 -0500

    debugging sms

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -256,7 +256,7 @@ sub input{
                                                  $txtmsg->{'conversation'}->{'room'} = $room;
                                                  $txtmsg->{'conversation'}->{'body'} = $sms;
                                                  $txtmsg->{'conversation'}->{'id'}   = $msg->{'conversation'}->{'id'};
-                                                 print STDERR Data::Dumper->Dump([ $txtmsg, $sms ]);
+                                                 print STDERR Data::Dumper->Dump([ $txtmsg->{'sender_alias'}, $respond_event, $txtmsg, "[$from] $sms" ]);
                                                  $kernel->post($txtmsg->{'sender_alias'}, $respond_event, $txtmsg, "[$from] $sms"); 
                                                  last;
                                                };
