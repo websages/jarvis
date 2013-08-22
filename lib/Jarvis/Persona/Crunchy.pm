25381b5538dda3a38e78f8683dfabbb7676744fe
James S White <james@jameswhite.org>
2013-08-22 16:25:22 -0500

    ive broken something

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -255,7 +255,7 @@ sub input{
                                                      warn $@ if $@;
 
                                               };
-            /flip\s+(.*)\s+or\s+(.*)\s*?*/   && do {
+            /flip\s+(.*)\s+or\s+(.*)\s*\?*/   && do {
                                                 print STDERR "$1 || $2\n";
                                                 my $range = 100;
                                                 my $random_number = int(rand($range));
