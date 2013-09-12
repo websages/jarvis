b473e1547bf735769c4848e9c750d96cf8541288
James S White <james@jameswhite.org>
2013-09-11 19:58:45 -0500

    adding \_/

--- lib/Jarvis/Persona/Crunchy.pm
+++ lib/Jarvis/Persona/Crunchy.pm
@@ -212,7 +212,7 @@ sub input{
             /global business excellence/i
                                         && do { $replies = [ "Unison!" ]; last; };
             /unison/i                   && do { $replies = [ "The Way To Global Business Excellence!" ]; last; };
-            /scotch/i                   && do {
+            /(scotch|\\_\/)/i           && do {
                                                 $pirate=0;
                                                 eval {
                                                        if($direct){
